4a704b63af11899324f3d5d56a059a2defec22bc
JeanPierre07 <jmansillaa@uni.pe>
2020-01-16 18:05:03 -0500

    v 1.0.x

--- changelog.md
+++ changelog.md
@@ -1,5 +1,15 @@
-# Hello world in Python
+    # Hello world in Python
 
 ## Version 1.0.0
+-  *readme.md*.
+
+## Version 1.0.1
+-    Added *class 1*.
+
+## Version 1.0.2
+-   Added *class 2*.
+
+## Version 1.0.3
+-    Added *class 3*.
 
 A simple _hello world_ script in Python
---
+++ clase_2/if_else.py
@@ -0,0 +1,55 @@
+x = 10 
+# Pedro tiene x años
+#print('Pedro tiene ',x,'años.')
+#print('Pedro tiene {} años.'.format(x))
+#print (fPedro titne {x} años.')
+#print(type(x))
+
+#x = 'Anthony'
+#print(type(x))
+
+#x = 10.1
+#print(type(x))
+
+# Operadores aritméticos
+#x = 10
+#print(x)
+#x += 5 #x = x + 5
+#print (x)
+#x -= 4 #x = x - 4
+#print(x)
+#x *= 3 #x = x*3
+#print(x)
+#x /= 5 # x = x/5
+#print (x)
+#x = x**3 # x al cubo
+#print(x)
+#y = x
+#print(y)
+#print (y)
+#x = x//89 #cociente / division entera
+#print (x)
+#x = x % 89 # residuo
+#print(x)
+#x = 10
+#x = 10**(0.5)
+#print(x)
+
+name = input ('What \'s your name : ')
+age = int (input('How old are you?: '))
+
+print ('Hi' , name , 'your name')
+print('You are', age , 'years old!')
+
+#if age >= 18:
+       #print('You are allowed to pass ')
+#else :
+       #print ('You shall not pass')
+if age < 0 :
+  print('Una edad no puede ser negativa') # an age can´t be negative
+elif age < 18 :   # elseif = elif
+  print ('No puedes pasar')   # You shall not pass
+else :
+    print ('Puedes pasar') #You are allowed to pass
+
+       
